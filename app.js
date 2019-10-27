9168b2caf9de1ce1e8864525200f93a8053123f9
Samir70 <sbetmouni@talktalk.net>
2019-10-27 09:53:25 +0000

    enable create/open and close the database

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
 const handleRatings = require('./routes/QRatings');
+
+// open database, default second parameter is: sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE
+let db = new sqlite3.Database('./database/QRatings.db', (err) => {
+    if (err) {
+        return console.error(err)
+    }
+    console.log('opened database')
+});
+
+db.close((err) => {
+    if (err) {
+        console.error(err)
+    }
+    console.log('database closed')
+});
+
 const app = express();
 app.use(express.json())
 
