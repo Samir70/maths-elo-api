0d4adf9f69f0d5c1389206ddf8d04cdd674591f0
Samir70 <sbetmouni@talktalk.net>
2019-10-28 12:10:56 +0000

    update, insert or get particular row of database

--- controllers/QRatings.js
+++ controllers/QRatings.js
@@ -2,16 +2,21 @@ const sqlite3 = require('sqlite3').verbose();
 
 // strange: the filename needs to have a path from app.js
 const fileName = './database/QRatings.db';
+const rating4unknownTypes = 1300;
 
-const getAllSQL = 'SELECT qType, rating FROM qratings ORDER BY qType';
+const getAllSQL = 'SELECT qType, rating FROM qratings ORDER BY rating';
+const get1RatingSQL = 'SELECT qType, rating FROM qratings WHERE qType = ?';
+const update1SQL = 'INSERT OR REPLACE INTO qratings (qType, rating) VALUES(?, ?)';
+
+const errCallback = (err, fromWhere) => {
+    if (err) {
+        return console.error('From ', fromWhere,err)
+    }
+    console.log('opened the database in ', fromWhere);
+}
 
 exports.getAllRatings = (req, res, next) => {
-    let db = new sqlite3.Database(fileName, (err) => {
-        if (err) {
-            return console.error('From getAllRatings',err)
-        }
-        console.log('opened the database in getAllRatings');
-    });
+    let db = new sqlite3.Database(fileName, (err) => errCallback(err, 'getAllRatings'));
     
     var dbResults = {};
     db.all(getAllSQL, [], (err, rows) => {
@@ -27,14 +32,41 @@ exports.getAllRatings = (req, res, next) => {
     });
 }
 
+exports.get1Rating = (req, res, next) => {
+    let db = new sqlite3.Database(fileName, (err)=>errCallback(err, 'get1Rating'));
+
+    const qType = req.body.qType || 'undefined qType';
+    db.get(get1RatingSQL, [qType], (err, row) => {
+        if (err) {throw err}
+        var found = row ? row.qType : 'nothing';
+        var rating2return = row ? row.rating : rating4unknownTypes;
+        res.status(200).json({found: found, rating: rating2return})
+    })
+
+    db.close((err) => {
+        if (err) { throw err }
+        console.log('get1Rating has closed the database.')
+    });
+}
+
 exports.updateRatings = (req, res, next) => {
     const category = req.body.category;
-    const value = req.body.ratingValue;
-    // update db
-    // 201 = success + resource created
-    res.status(201).json({
-        message: 'received new data',
-        category: category,
-        QRating: value
-    })
+    const value = req.body.ratingValue || rating4unknownTypes;
+    const tryToInsertSQL = 'INSERT INTO qratings(qtype, rating) VALUES(?, ?)';
+    
+    let db = new sqlite3.Database(fileName, (err) => errCallback(err, 'updateRatings'));
+    db.run(update1SQL, [category, value], (err)=> {
+              if (err) {return console.log(err)}
+              res.status(201).json({
+                  message: 'received new data',
+                  category: category,
+                  QRating: value,
+              });
+    }); 
+    
+
+    db.close((err) => {
+        if (err) { throw err }
+        console.log('updateRatings has closed the database.')
+    });
 }
--- routes/QRatings.js
+++ routes/QRatings.js
@@ -4,6 +4,7 @@ const ratingsController = require('../controllers/QRatings');
 handleRatings = express.Router();
 
 handleRatings.get('/all', ratingsController.getAllRatings);
+handleRatings.get('/get1rating', ratingsController.get1Rating);
 handleRatings.post('/new-data', ratingsController.updateRatings);
 
 module.exports = handleRatings;
