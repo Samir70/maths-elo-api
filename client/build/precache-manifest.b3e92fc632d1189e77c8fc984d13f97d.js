10cfccc3a3f70720591dad59932dbf1cf8a91d66
Samir70 <sbetmouni@talktalk.net>
2019-10-29 16:08:19 +0000

    add the client/build folder to git push

---
+++ client/build/precache-manifest.b3e92fc632d1189e77c8fc984d13f97d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618acee41db4edcb7ba2d99ad4b14667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6452e8b657686c91c18d",
+    "url": "/static/css/main.f5dad7b7.chunk.css"
+  },
+  {
+    "revision": "2b3d9d1d1c912cd09ad7",
+    "url": "/static/js/2.3fafab12.chunk.js"
+  },
+  {
+    "revision": "6452e8b657686c91c18d",
+    "url": "/static/js/main.c270c223.chunk.js"
+  },
+  {
+    "revision": "364e15a5c0fa32446e4d",
+    "url": "/static/js/runtime~main.2868dd57.js"
+  }
+]);
