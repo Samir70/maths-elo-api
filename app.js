df8515dd6c01a9db1064ede15d8d8e06e2840ad9
Samir70 <sbetmouni@talktalk.net>
2019-10-29 14:31:33 +0000

    include heroku PORT option for app.listen

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const sqlite3 = require('sqlite3').verbose();
 
 const handleRatings = require('./routes/QRatings');
 
+const port = process.env.PORT || 8080
 
 const app = express();
 app.use(express.json())
 
 app.use('/qratings', handleRatings);
-app.listen(8080);
+app.listen(port);
