b18a6cbec73688e9dc9bc2d94eb2a6bc6d5e9377
Samir70 <sbetmouni@talktalk.net>
2019-10-30 11:48:47 +0000

    begin change to postgresql

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const { Client } = require('pg');
 
 const handleRatings = require('./routes/QRatings');
 
 const port = process.env.PORT || 8080
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl : true
+});
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+});
 
 const app = express();
 app.use(express.json())
