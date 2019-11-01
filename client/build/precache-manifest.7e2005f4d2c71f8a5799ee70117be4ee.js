58c1ba69057a6a1daa034381e1d3e1ddedd1c6bf
Samir70 <sbetmouni@talktalk.net>
2019-11-01 05:57:15 +0000

    add ignored build files

---
+++ client/build/precache-manifest.7e2005f4d2c71f8a5799ee70117be4ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcda9c17efafa91da6a69a9e732b57db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7f34d11e5587e9e426a",
+    "url": "/static/css/main.f5dad7b7.chunk.css"
+  },
+  {
+    "revision": "4f2ac7f5bcd815d65d33",
+    "url": "/static/js/2.47a9fe39.chunk.js"
+  },
+  {
+    "revision": "f7f34d11e5587e9e426a",
+    "url": "/static/js/main.d4a7bcac.chunk.js"
+  },
+  {
+    "revision": "364e15a5c0fa32446e4d",
+    "url": "/static/js/runtime~main.2868dd57.js"
+  }
+]);
