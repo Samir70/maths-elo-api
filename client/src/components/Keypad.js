bb8560f5f896e73e889c2a4e9ad8a8949ebc24b5
Samir70 <sbetmouni@talktalk.net>
2019-11-17 09:48:11 +0000

    style the keypad

--- client/src/components/Keypad.js
+++ client/src/components/Keypad.js
@@ -6,26 +6,22 @@ import { Keypad_number, Keypad_delete } from '../Reducers/actions';
 const Keypad = ({ Keypad_number, Keypad_delete }) => {
     const buttons1_9 = [...'123456789'].map(x => <button
         onClick={() => Keypad_number(x)}
-        accessKey={x}
         className="keypadButton"
         key={'keypad-button' + x}>{x}</button>);
     const button0 = <button
         onClick={() => Keypad_number('0')}
-        accessKey='0'
         className="keypadButton"
         key='keypad-button0'>0</button>;
     const buttonDP = <button
         onClick={() => Keypad_number('.')}
-        accessKey='.'
         className="keypadButton"
         key='keypad-buttonDP'>.</button>
     const buttonDEL = <button
         onClick={() => Keypad_delete()}
-        accessKey='-'
         className="keypadButton"
         key='keypad-buttonDEL'>DEL</button>
     return (
-        <div>
+        <div className="keypad">
             <div>{buttons1_9.slice(0, 3)}</div>
             <div>{buttons1_9.slice(3, 6)}</div>
             <div>{buttons1_9.slice(6)}</div>
