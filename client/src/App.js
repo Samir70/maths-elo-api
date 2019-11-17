cd9a169ccf44f1e52a30d7280d5415e18a440671
Samir70 <sbetmouni@talktalk.net>
2019-11-17 08:17:48 +0000

    show ???? if qRating unknown

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,8 @@ const App = ({ userRating, currentQ, showClassRoom, ToggleClass }) => {
           {showClassRoom ? rateMymathsOption : giveme5Option}
         </DropdownButton>
         <p>Rate and review your maths</p>
-        <p className="userVSquestion" >Guest User ({userRating}) VS {currentQ.QType} ({currentQ.QRating}) </p>
+        <p className="userVSquestion" >Guest User ({userRating}) VS 
+          {currentQ.QType} ({currentQ.QRating||'????'}) </p>
       </div>
       {showClassRoom ? <ClassRoom QType={currentQ.QType} /> : <Question />}
     </div>
