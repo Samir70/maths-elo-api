8f05d7a642406424bfe28a4e071f093b4725e4de
Samir70 <sbetmouni@talktalk.net>
2019-12-01 08:51:23 +0000

    add the build

---
+++ client/build/precache-manifest.73ffb7fee29b97cd848d14887cbcfb19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a8a6856fa19838c7bd8842b8a2a20d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d19990aba05ebf4bc63",
+    "url": "/static/css/main.f5dad7b7.chunk.css"
+  },
+  {
+    "revision": "71a2326e434801a36b03",
+    "url": "/static/js/2.6031a42f.chunk.js"
+  },
+  {
+    "revision": "1d19990aba05ebf4bc63",
+    "url": "/static/js/main.552d9d97.chunk.js"
+  },
+  {
+    "revision": "364e15a5c0fa32446e4d",
+    "url": "/static/js/runtime~main.2868dd57.js"
+  }
+]);
