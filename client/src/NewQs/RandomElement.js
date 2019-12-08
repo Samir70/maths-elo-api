c02bb67e38038cfb0cb85008965496901d0889ca
Samir70 <sbetmouni@talktalk.net>
2019-12-08 21:41:23 +0000

    extract randomInt func

--- client/src/NewQs/RandomElement.js
+++
@@ -1,3 +0,0 @@
-const RandomElement = (arr) => arr[Math.floor(Math.random()*arr.length)];
-
-export default RandomElement;
---
+++ client/src/NewQs/RandomFuncs.js
@@ -0,0 +1,4 @@
+export const RandomElement = (arr) => arr[Math.floor(Math.random()*arr.length)];
+export const RandomInt = (n) => Math.floor(Math.random()*n);
+
+//export default RandomElement;
