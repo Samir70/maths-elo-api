f368adfcaaebbacebb5bbaff2430af4fb87da133
Samir70 <sbetmouni@talktalk.net>
2019-12-26 10:06:33 +0000

    add randomPrime function

--- client/src/NewQs/RandomFuncs.js
+++ client/src/NewQs/RandomFuncs.js
@@ -1,6 +1,11 @@
 const RandomElement = (arr) => arr[Math.floor(Math.random()*arr.length)];
 const RandomInt = (n) => Math.floor(Math.random()*n);
 
-module.exports = {RandomElement, RandomInt}
+const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
+const smallPrimes = [2, 3, 5, 7];
+
+const RandomPrime = (t) => t === 'small' ? RandomElement(smallPrimes) : RandomElement(primes);
+
+module.exports = {RandomElement, RandomInt, RandomPrime}
 
 //export default RandomElement;
