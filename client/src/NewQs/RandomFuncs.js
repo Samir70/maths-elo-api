1d91eb99a375f6b3a10654ed7bfafcde263ebf4b
Samir70 <sbetmouni@talktalk.net>
2019-12-26 18:54:39 +0000

    add distinct random elements function

--- client/src/NewQs/RandomFuncs.js
+++ client/src/NewQs/RandomFuncs.js
@@ -1,11 +1,22 @@
 const RandomElement = (arr) => arr[Math.floor(Math.random()*arr.length)];
 const RandomInt = (n) => Math.floor(Math.random()*n);
 
+const distinctElementsFrom = (n, arr) => {
+    var list = [];
+    if (arr.length<=n) {return arr}
+    while (list.length<n) {
+        list.push(RandomElement(arr.filter(x=>list.indexOf(x)===-1)))
+    }
+    return list
+}
+
 const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
 const smallPrimes = [2, 3, 5, 7];
 
 const RandomPrime = (t) => t === 'small' ? RandomElement(smallPrimes) : RandomElement(primes);
+const distinctPrimes = (n, t) => t === 'small' ? 
+            distinctElementsFrom(n, smallPrimes) : distinctElementsFrom(n, primes);
 
-module.exports = {RandomElement, RandomInt, RandomPrime}
+module.exports = {RandomElement, RandomInt, RandomPrime, distinctElementsFrom, distinctPrimes}
 
 //export default RandomElement;
