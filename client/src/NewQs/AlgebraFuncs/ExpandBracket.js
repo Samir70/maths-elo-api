0bfc970f93c668246ae757be4fe23559ffa26257
Samir70 <sbetmouni@talktalk.net>
2019-12-26 19:38:59 +0000

    add expand, simplify and factorise questions

--- client/src/NewQs/AlgebraFuncs/ExpandBracket.js
+++ client/src/NewQs/AlgebraFuncs/ExpandBracket.js
@@ -1,5 +1,5 @@
 import { Coeffs } from './CoeffsAndVars';
-import { RandomInt } from '../RandomFuncs';
+import { RandomInt, distinctPrimes } from '../RandomFuncs';
 import { combine } from './SimplifyAddSub';
 
 export const Brackets1 = (outer, coeffs, varNames) => {
@@ -17,21 +17,35 @@ export const Expand1Q = () => {
         a: Expand1(mult, co, ['a', ''])
     }
 }
+export const FactoriseQ = (level) => {
+    var co = [];
+    var [p1, p2] = distinctPrimes(2, 'small');
+    switch (level) {
+        case 1: { co = RandomInt(2) === 0 ? [1, p1] : [p1, 1]; break }
+        case 2: { co = RandomInt(2) === 0 ? [p1, p2] : [p1, -p2]; break }
+        default: { console.log('level unknown in FactoriseQ:', level); co = [3, 2] }
+    }
+    var out = RandomInt(9) + 2;
+    return {
+        q: 'Factorise ' + Expand1(out, co, ['a', 'b']),
+        a: Brackets1(out, co, ['a', 'b'])
+    }
+}
 
 // brackets always contain same variable names in same order
 export const Expand2 = (out1, co1, pm, out2, co2, varNames) => {
     var first = multiply(out1, co1), second = multiply(out2, co2);
-    var final = first.map((x, i) => pm === '+' ? x+second[i] : x - second[i]);
+    var final = first.map((x, i) => pm === '+' ? x + second[i] : x - second[i]);
     return combine(final, varNames)
 }
 
 export const Expand2SimplifyQ = () => {
     var c1 = Coeffs(2), c2 = Coeffs(2);
-    var o1 = RandomInt(9)+2, o2 = RandomInt(9)+2;
-    var pm = RandomInt(2)===0 ? '+' : '-';
+    var o1 = RandomInt(9) + 2, o2 = RandomInt(9) + 2;
+    var pm = RandomInt(2) === 0 ? '+' : '-';
     var vs = ['a', 'b']
     return {
-        q:'Expand and simplify '+Brackets1(o1, c1, vs)+pm+Brackets1(o2, c2, vs),
-        a:Expand2(o1, c1, pm, o2, c2, vs)
+        q: 'Expand and simplify ' + Brackets1(o1, c1, vs) + pm + Brackets1(o2, c2, vs),
+        a: Expand2(o1, c1, pm, o2, c2, vs)
     }
 }
--- client/src/NewQs/AlgebraQ.js
+++ client/src/NewQs/AlgebraQ.js
@@ -1,27 +1,56 @@
 import { Algebra01 } from './QTypes';
 import { RandomElement } from './RandomFuncs';
 import { simpAddSubQ } from './AlgebraFuncs/SimplifyAddSub';
- 
+import { Expand1Q, Expand2SimplifyQ, FactoriseQ } from './AlgebraFuncs/ExpandBracket';
+
 const subQTypes = [
-    null, 'simplifyAddSub', 'expandBracket'
+    null, 'simplifyAddSub', 'expandBracket', 'expand2Simplify', 'factorise1', 'factorise2'
 ];
 
 const Algebra01Q = (subType) => {
-    const subQType = subQTypes[Number(subType)] || RandomElement(subQTypes.slice(1));
-    var quAndA = { 
-        QType: Algebra01+'-'+subQType, 
-        extraKeys:['a', 'b', 'c', '+', '-', '/'],
-        furtherInstructions: 'Give variables in alphabetical order. eg: 7+3a-2b' 
+    const subQType = 'factorise2'//subQTypes[Number(subType)] || RandomElement(subQTypes.slice(1));
+    var quAndA = {
+        QType: Algebra01 + '-' + subQType,
+        extraKeys: ['a', 'b', 'c', '+', '-', '/'],
     }
+    var newQ = {};
     switch (subQType) {
-        case 'simplifyAddSub' : {
-            var newQ = simpAddSubQ(5);
-            quAndA.q=newQ.q; quAndA.a=newQ.a;
-            quAndA.answerFormat='string'; 
+        case 'simplifyAddSub': {
+            newQ = simpAddSubQ(5);
+            quAndA.answerFormat = 'string';
+            quAndA.furtherInstructions = 'Give variables in alphabetical order. eg: 7+3a-2b'
             break
         }
-        default : {quAndA.q = 'default algebra01 Q'; quAndA.a=42}
+        case 'expandBracket': {
+            newQ = Expand1Q();
+            quAndA.answerFormat = 'string';
+            quAndA.furtherInstructions = 'Give variables in order of appearance in brackets';
+            break
+        }
+        case 'expand2Simplify' : {
+            newQ = Expand2SimplifyQ();
+            quAndA.answerFormat = 'string';
+            quAndA.furtherInstructions = 'Give variables in order of appearance in brackets';
+            break
+            
+        }
+        case 'factorise1' : {
+            newQ = FactoriseQ(1);
+            quAndA.answerFormat = 'string';
+            quAndA.furtherInstructions = 'Give variables in order of appearance';
+            quAndA.extraKeys = ['a', 'b', '(', '+', '-', ')'];
+            break
+        }
+        case 'factorise2' : {
+            newQ = FactoriseQ(2);
+            quAndA.answerFormat = 'string';
+            quAndA.furtherInstructions = 'Give variables in order of appearance';
+            quAndA.extraKeys = ['a', 'b', '(', '+', '-', ')'];
+            break
+        }
+        default: { newQ.q = 'default algebra01 Q'; newQ.a = 42 }
     }
+    quAndA.q = newQ.q; quAndA.a = newQ.a;
     return quAndA;
 }
 
