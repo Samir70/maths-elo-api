4d1b2dc424bd83c131ca15e28d23ff80959ebb84
Samir70 <sbetmouni@talktalk.net>
2020-01-02 19:49:05 +0000

    create blank login form

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+const LoginForm = () => {
+    return (
+        <div>
+            <p>This is the login form</p>
+        </div>
+    )
+}
+
+export default LoginForm;
