2afb9c967b2bad1cc8375fe7a2d8282045c3c2eb
Samir70 <sbetmouni@talktalk.net>
2020-01-18 09:33:33 +0000

    display list of topics with active status shown

--- client/src/components/TopicSelect.js
+++ client/src/components/TopicSelect.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { QTypes } from '../NewQs/QTypes'
 
 const TopicSelect = () => {
+    const selectedTopics = ['Ratio'];
+    const qTypeList = QTypes.map(q =>
+        <ListGroup.Item
+            action
+            variant={ selectedTopics.includes(q) ? "success" : "secondary" }
+            key={q}>{q}
+        </ListGroup.Item>
+    )
     return (
-        <h2>Select Topics</h2>
+        <div>
+            <h2>Select Topics</h2>
+            <p>Topics highlighted in green are active. Click to toggle</p>
+            <ListGroup id="topicList">{qTypeList}</ListGroup>
+        </div>
     )
 }
 
