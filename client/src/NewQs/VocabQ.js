439db459b2132a15a66c6df544e8697733c169f0
Samir70 <sbetmouni@talktalk.net>
2020-01-18 13:41:25 +0000

    correct grammar in vocab test

--- client/src/NewQs/VocabQ.js
+++ client/src/NewQs/VocabQ.js
@@ -6,7 +6,7 @@ const VocabQList = [
      a:'diameter'},
     {q:'What do you call a line from the centre of a circle to the edge of the circle?', a:'radius'},
     {q:'What do you call a line that just touches the outside of a circle?', a:'tangent'},
-    {q:'What do you call a line cuts across a circle, but not necessarily through the centre?', a:'chord'},
+    {q:'What do you call a line that cuts across a circle, but not necessarily through the centre?', a:'chord'},
     {q:'A chord cuts a circle into two ...?', a:'segments'},
     {q:'Two radii (radiuses) cut out a piece like a slice of pie, called a ...?', a:'sector'}
 ]
