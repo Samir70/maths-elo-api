94a5b945e7eb14b747abd1c273e9bf42cc2330e8
Samir70 <sbetmouni@talktalk.net>
2020-01-18 13:57:59 +0000

    added more vocab questions

--- client/src/NewQs/VocabQ.js
+++ client/src/NewQs/VocabQ.js
@@ -1,7 +1,6 @@
 import { Vocab } from './QTypes';
 
-const VocabQList = [
-    // circle vocab
+const circleVocab = [
     {q:'What do you call a line that goes from the edge of a circle, through the centre, to the other side?', 
      a:'diameter'},
     {q:'What do you call a line from the centre of a circle to the edge of the circle?', a:'radius'},
@@ -9,7 +8,26 @@ const VocabQList = [
     {q:'What do you call a line that cuts across a circle, but not necessarily through the centre?', a:'chord'},
     {q:'A chord cuts a circle into two ...?', a:'segments'},
     {q:'Two radii (radiuses) cut out a piece like a slice of pie, called a ...?', a:'sector'}
+];
+
+const triangleVocab = [
+    {q:'A triangle with all sides equal is described as ...?', a:'equilateral'},
+    {q:'A triangle with exactly two sides equal is described as ...?', a:'isosceles'},
+    {q:'A triangle with all sides of different length is described as ...?', a:'scalene'},
+    {q:'The long side of a right-angled triangle is called the ...?', a:'hypotenuse'}
+];
+
+const angleVocab = [
+    {q:'An angle which is less than 90 degrees is described as ...?', a:'acute'},
+    {q:'An angle which is exactly 90 degrees is described as ...?', a:'right'},
+    {q:'An angle which is more than 90 degrees is described as ...?', a:'obtuse'},
+    {q:'An angle which is more than 180 degrees is described as ...?', a:'reflex'},
+    {q:'Two angles which add up to 180 degress are described as ...?', a:'supplementary'},
+    {q:'Two angles which add up to 90 degress are described as ...?', a:'complementary'},
+    {q:'Lines which point in the same direction and never meet are described as ...?', a:'parallel'}
 ]
+
+const VocabQList = [...circleVocab, ...triangleVocab, ...angleVocab];
 
 const VocabQ = () => {
     var question = Math.floor(Math.random()*VocabQList.length);
