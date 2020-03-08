884e6bd12389fde0e888681c441bbf8260c31844
Samir70 <sbetmouni@talktalk.net>
2020-03-08 21:22:17 +0000

    add decimal rounding question

--- client/src/NewQs/Decimals.js
+++ client/src/NewQs/Decimals.js
@@ -2,7 +2,7 @@ import { Decimal } from './QTypes';
 import { RandomInt, RandomElement } from './RandomFuncs';
 
 const subQTypes = [
-    null, 'order'
+    null, 'order', 'round'
 ];
 
 const orderQ = () => {
@@ -23,6 +23,33 @@ const orderQ = () => {
     }
 }
 
+const roundQ = (dPlaces) => {
+    var dToRound = [...'012345'].map((x, i)=> i===2 ? '.' : RandomInt(9)+1).join('');
+    switch (dPlaces) {
+        case 0 : {
+            return {
+                q:'Round '+dToRound+' to the nearest whole number',
+                a: ''+Math.round(dToRound)
+            }
+        }
+        case 1 : {
+            return {
+                q:'Round '+dToRound+' to one decimal place',
+                a:''+Number(Math.round(dToRound+'e1')+'e-1').toFixed(1)
+            }
+        }
+        case 2 : {
+            return {
+                q:'Round '+dToRound+' to two decimal place',
+                a:''+Number(Math.round(dToRound+'e2')+'e-2').toFixed(2)
+            }
+        }
+        default : {
+            return {q:'Round 0.42 to one decimal place', a:'0.4'}
+        }
+    }
+}
+
 const DecimalQ = (subType) => {
     const subQType = subType || RandomElement(subQTypes.slice(1));
     var quAndA = { QType: Decimal + '-' + subQType };
@@ -31,7 +58,14 @@ const DecimalQ = (subType) => {
         case 'order' : { 
             qA = orderQ(); 
             quAndA.furtherInstructions='give numbers seperated by comma and space';
-            quAndA.answerFormat='string'; break }
+            quAndA.answerFormat='string'; 
+            break 
+        }
+        case 'round' : {
+            qA = roundQ(RandomInt(3));
+            quAndA.answerFormat='string'; 
+            break
+        }
         default : {  
             qA.q = 'default Decimal Question';
             qA.a = 42
