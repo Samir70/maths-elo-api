b34fdac8fef988910fc93646af0575457a2ff9ff
Samir70 <sbetmouni@talktalk.net>
2021-01-03 20:49:37 +0000

    add simulEqs needs arranging

--- client/src/NewQs/simulEqsQ.js
+++ client/src/NewQs/simulEqsQ.js
@@ -18,43 +18,52 @@ const simulEqsQ = (subType) => {
     let x = RandomInt(8) + 2; // 2, 3, 4, 5, 6, 7, 8, 9
     let y = RandomInt(8) + 2; // 2, 3, 4, 5, 6, 7, 8, 9
     if (x === y) { y++ }
-    let coefx1 = RandomInt(8) + 2;
-    let coefy1 = RandomInt(8) + 2;
-    let coefy2 = RandomInt(8) + 2;
-    if (coefy1 === coefy2) { coefy2++ }
+    let coef1 = RandomInt(8) + 2;
+    let coef2 = RandomInt(8) + 2;
+    let coef3 = RandomInt(8) + 2;
+    if (coef2 === coef3) { coef3++ }
     switch (subQType) {
         case 'shopping' : {
-            qA.q = `One shopper buys ${coefx1} teas and ${coefy1} buns. 
-            While a second shopper buys ${coefx1} teas and ${coefy2} buns. 
-            The first shopper pays £${coefx1*x + coefy1*y}, while the second pays £${coefx1*x + coefy2*y}.
+            qA.q = `One shopper buys ${coef1} teas and ${coef2} buns. 
+            While a second shopper buys ${coef1} teas and ${coef3} buns. 
+            The first shopper pays £${coef1*x + coef2*y}, while the second pays £${coef1*x + coef3*y}.
             How much does a tea cost?`;
             qA.a = '£'+x;
             break
         }
         case 'sameOfOne' : {
             qA.q = RandomInt(2) === 0 ? 
-                `Solve ${coefx1}A + ${coefy1}B = ${coefx1*x + coefy1*y} 
-                and ${coefx1}A + ${coefy2}B = ${coefx1*x + coefy2*y}` : 
-                `Solve ${coefy1}A + ${coefx1}B = ${coefy1*x + coefx1*y} 
-                and ${coefy2}A + ${coefx1}B = ${coefy2*x + coefx1*y}` 
+                `Solve ${coef1}A + ${coef2}B = ${coef1*x + coef2*y} 
+                and ${coef1}A + ${coef3}B = ${coef1*x + coef3*y}` : 
+                `Solve ${coef2}A + ${coef1}B = ${coef2*x + coef1*y} 
+                and ${coef3}A + ${coef1}B = ${coef3*x + coef1*y}` 
             qA.a = `A = ${x} and B = ${y}`
             break
         }
         case 'negOfOne' : {
             qA.q = RandomInt(2) === 0 ? 
-                `Solve ${coefx1}A + ${coefy1}B = ${coefx1*x + coefy1*y} 
-                and ${coefy2}B - ${coefx1}A = ${coefy2*y - coefx1*x}` : 
-                `Solve ${coefy1}A - ${coefx1}B = ${coefy1*x - coefx1*y} 
-                and ${coefy2}A + ${coefx1}B = ${coefy2*x + coefx1*y}` 
+                `Solve ${coef1}A + ${coef2}B = ${coef1*x + coef2*y} 
+                and ${coef3}B - ${coef1}A = ${coef3*y - coef1*x}` : 
+                `Solve ${coef2}A - ${coef1}B = ${coef2*x - coef1*y} 
+                and ${coef3}A + ${coef1}B = ${coef3*x + coef1*y}` 
             qA.a = `A = ${x} and B = ${y}`
             break
         }
         case 'needsScaling' : {
+            let coef4 = coef2 === 2 ? 3 : coef2 - 1;
             qA.q = RandomInt(2) === 0 ? 
-            `Solve A + B = ${x + y} 
-            and ${coefy2}B - ${coefx1}A = ${coefy2*y - coefx1*x}` : 
-            `Solve 2A + ${coefy1}B = ${2*x + coefy1*y} 
-            and ${coefx1}A + ${coefy2}B = ${coefx1*x + coefy2*y}`
+            `Solve ${2*coef1}A + ${coef3}B = ${2*coef1*x + coef3*y} 
+            and ${coef2}B - ${coef1}A = ${coef2*y - coef1*x}` : 
+            `Solve ${coef1}A + ${coef2}B = ${coef1*x + coef2*y} 
+            and ${coef1*2}A + ${coef4}B = ${coef1*2*x + coef4*y}`
+            qA.a = `A = ${x} and B = ${y}`
+            break
+        }
+        case 'needsArranging' : {
+            let c = coef1*x - coef3*y;
+            qA.q = 'Solve ';
+            qA.q += c < 0 ? `${coef1}A = ${coef3}B - ${-c}` : `${coef1}A - ${c} = ${coef3}B`
+            qA.q += ` and ${coef1}A + ${coef2}B = ${coef1*x + coef2*y}`
             qA.a = `A = ${x} and B = ${y}`
             break
         }
