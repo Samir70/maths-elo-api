e2b1656ae2915fe7fd63a688e83b64ceefa93049
Samir70 <sbetmouni@talktalk.net>
2021-03-20 22:44:19 +0000

    improve notation for nthTerm expression

--- client/src/NewQs/sequenceFuncs.js/linearSeqQs.js
+++ client/src/NewQs/sequenceFuncs.js/linearSeqQs.js
@@ -1,10 +1,11 @@
-import {RandomInt} from '../RandomFuncs';
+import { RandomInt } from '../RandomFuncs';
+import { combine } from '../AlgebraFuncs/SimplifyAddSub';
 
 const rndLinearSeq = n => {
     let d = RandomInt(10) + 2
     let a = RandomInt(30) + 5
-    let seq = [...Array(n).fill(0)].map((_, i) => a + i*d)
-    return {seq, a, d}
+    let seq = [...Array(n).fill(0)].map((_, i) => a + i * d)
+    return { seq, a, d }
 }
 
 export const nextTermLinear = () => {
@@ -12,13 +13,14 @@ export const nextTermLinear = () => {
     return {
         q: 'Find the next term in ' + seq.slice(0, 5).join(', ') + ', ...',
         a: seq[5]
-    }    
+    }
 }
 
 export const nthTermQ = () => {
-    let {seq, a, d} = rndLinearSeq(5)
+    let { seq, a, d } = rndLinearSeq(5)
+    let expression = combine([d, a-d], ['n', ''])
     return {
-        q : 'Find the first 5 terms in the sequence with nth term formula: \n'+d+'n'+' + '+(a -d),
+        q: 'Find the first 5 terms in the sequence with nth term formula: \n' + expression,
         a: seq.join(', ')
     }
 }
